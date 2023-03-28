b01dc66bbde511f4f7b6631acf3d9be2c9afda16
Jay Morgan <jaymiles17@gmail.com>
2023-03-28 12:54:57 +0100

    Add day 4

--- 2022/lib.scm
+++ 2022/lib.scm
@@ -9,6 +9,9 @@
 	      (list->string (reverse contents))
 	      (loop (cons current-line contents))))))))
 
+(define (read-file-lines filepath)
+  (filter (lambda (str) (not (string= str ""))) (string-split (read-file filepath) #\newline)))
+
 ;; Take N elements from LST.
 ;; i.e.
 ;; (take 3 '(a b c d e f)) => '(a b c)
@@ -34,3 +37,24 @@
 (define (rest lst) (cdr lst))
 
 (define (sum lst) (apply + lst))
+
+
+(define (range start stop step)
+  (let loop ([out (list)] [i start])
+    (cond ((>= i stop) (reverse out))
+	  (else (loop (cons i out) (+ i step))))))
+
+;; unique. Given a list find the unique elements of that list.
+(define (unique lst)
+  (let loop ([it-lst lst] [els (list)])
+    (cond [(null? it-lst) (reverse els)]
+	  [(member (first it-lst) els) (loop (rest it-lst) els)]
+	  [else (loop (rest it-lst) (cons (first it-lst) els))])))
+
+;; union. Find the common elements between two sets.
+(define (intersection lst1 lst2)
+  (define (f lst1 lst2)
+    (cond ((or (null? lst1) (null? lst2)) (list))
+	  ((member (car lst1) lst2) (cons (car lst1) (f (cdr lst1) lst2)))
+	  (else (f (cdr lst1) lst2))))
+  (f lst1 lst2))
